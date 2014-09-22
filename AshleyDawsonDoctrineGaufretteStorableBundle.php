4cb109db9ba5700f1dd9a3dbc3696f610ab7234b
Ashley <a.dawson@ashleydawson.co.uk>
2014-09-22 15:26:41 +0100

    Added bundle class

---
+++ AshleyDawsonDoctrineGaufretteStorableBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AshleyDawson\DoctrineGaufretteStorableBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class AshleyDawsonDoctrineGaufretteStorableBundle
+ * @package AshleyDawson\DoctrineGaufretteStorableBundle
+ *
+ * @author Ashley Dawson <ashley@ashleydawson.co.uk>
+ */
+class AshleyDawsonDoctrineGaufretteStorableBundle extends Bundle
+{
+
+}
