adc9dede14fb5ef06c11e8af6510dbdbaae3d3e2
Ashley <a.dawson@ashleydawson.co.uk>
2014-09-23 20:25:04 +0100

    Added better comments to storage event name constants

--- src/AshleyDawson/DoctrineGaufretteStorableBundle/Event/StorageEvents.php
+++ src/AshleyDawson/DoctrineGaufretteStorableBundle/Event/StorageEvents.php
@@ -12,21 +12,25 @@ class StorageEvents
 {
     /**
      * Pre write event name
+     * @see \AshleyDawson\DoctrineGaufretteStorableBundle\Event\WriteUploadedFileEvent
      */
     const PRE_WRITE = 'ad_doctrine_gaufrette_storable.pre_write';
 
     /**
      * Post write event name
+     * @see \AshleyDawson\DoctrineGaufretteStorableBundle\Event\WriteUploadedFileEvent
      */
     const POST_WRITE = 'ad_doctrine_gaufrette_storable.post_write';
 
     /**
      * Pre delete event name
+     * @see \AshleyDawson\DoctrineGaufretteStorableBundle\Event\DeleteUploadedFileEvent
      */
     const PRE_DELETE = 'ad_doctrine_gaufrette_storable.pre_delete';
 
     /**
      * Post delete event name
+     * @see \AshleyDawson\DoctrineGaufretteStorableBundle\Event\DeleteUploadedFileEvent
      */
     const POST_DELETE = 'ad_doctrine_gaufrette_storable.post_delete';
 }
